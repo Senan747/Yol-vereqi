e07a3ef80e02a134a9d5a0a6d0e4400935b3af00
Senan747 <111648854+Senan747@users.noreply.github.com>
2023-10-07 15:44:25 +0400

    first

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Main from "./components/Main.vue";
+import Map from "./components/Map.vue";
+</script>
+
+<template>
+  <div>
+    <Main />
+    <!-- <Map /> -->
+    <!-- <div id="components-demo">
+      <map-component></map-component>
+    </div> -->
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Main.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <div>
+        <select v-model="selectedYear">
+          <option value="2021">2021</option>
+          <option value="2022">2022</option>
+          <option value="2023">2023</option>
+        </select>
+      </div>
+      <div id="chart" class="chart-lg"></div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted, watch } from "vue";
+import ApexCharts from "apexcharts";
+import data from "../../db.json";
+
+const chart = ref(null);
+const selectedYear = ref("2021");
+
+onMounted(() => {
+  createChart(selectedYear.value);
+});
+
+watch(selectedYear, (newYear) => {
+  createChart(newYear);
+});
+
+function createChart(year) {
+  const options = {
+    chart: {
+      type: "line",
+    },
+    series: data.map((item) => ({
+      name: item.car,
+      data: selectedYear === "2021" ? item.year[selectedYear] : item.year[year],
+    })),
+    xaxis: {
+      categories: [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ],
+    },
+  };
+
+  if (chart.value) {
+    chart.value.destroy();
+  }
+
+  chart.value = new ApexCharts(document.querySelector("#chart"), options);
+  chart.value.render();
+}
+</script>
+
+<style scoped>
+.card-body {
+  max-width: 100vw;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: center;
+  padding-left: 70px;
+}
+
+.read-the-docs {
+  color: #888;
+}
+#chart {
+  min-width: 650px;
+  margin-top: 40px;
+}
+</style>
